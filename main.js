287147a774e666c9fb9c7095b5cc905ac48523ce
Devansh-Balani8 <100129107+Devansh-Balani8@users.noreply.github.com>
2022-06-02 17:46:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ function new_image()
     block_image_object.scaleToWidth(700);
     block_image_object.scaleToHeight(510);
     block_image_object.set({
-    top:10,
-    left:10
+    top:0,
+    left:0
 	});
     canvas.add(block_image_object)
 	});
